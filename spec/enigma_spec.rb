7557d503db4eedb82ceeb059e1ef061a2ec82da0
matthewjholmes <ilookup@gmail.com>
2021-08-09 21:42:42 -0600

    Rename files and require statements

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,5 +1,4 @@
 require_relative 'spec_helper'
-require 'date'
 
 RSpec.describe Enigma do
   before(:each) do
@@ -7,7 +6,7 @@ RSpec.describe Enigma do
     @encryption_method_all_args = @enigma.encrypt("hello world", "02715", "060821")
     @encryption_method_string_only = @enigma.encrypt("hello world")
     @encryption_method_string_and_key = @enigma.encrypt("hello world", "02715")
-
+    allow_any_instance_of(Enigma).to receive(:rand).and_return(12345)
   end
 
   describe '#initalize' do
@@ -17,27 +16,22 @@ RSpec.describe Enigma do
   end
 
   describe '#encrpyt' do
-    # key and date arguments should be optional
     it 'can #encrypt(message, key, date)' do
       expect(@encryption_method_all_args).to be_a(Hash)
       expect(@encryption_method_all_args).to eq({encryption: "nefau qdxly", key: "02715", date: "060821"})
     end
 
-    xit '#encrypt method takes key and date as optional' do
+    it '#encrypt method takes key and date as optional' do
       # need mock/stub for key and date generation
       expect(@encryption_method_string_only).to eq({encryption: "nefau qdxly", key: "02715", date: "060821"})
 
       expect(@encryption_method_string_and_key).to eq({encryption: "nefau qdxly", key: "02715", date: "060821"})
     end
 
-    it '#text_encrypter(message) applys shifts' do
-      # need mock/stub for key and date generation
-      expect(@enigma.text_encrypter("hello world")).to eq("nefau qdxly")
-    end
   end
 
   describe '#decrypt' do
-    xit 'can #decrypt(cyphertext, key, date)' do
+    xit 'can #decrypt(ciphertext, key, date)' do
       expect(@enigma.decrypt("nefau qdxly", "02715", "060821")).to eq({decryption: "hello world", key: "02715", date: "060821"})
     end
 
@@ -46,18 +40,10 @@ RSpec.describe Enigma do
       encrypted = @enigma.encrypt("hello world", "02715")
       expect(@enigma.decrypt(encrypted[:encryption], "02715")).to eq({decryption: "hello world", key: "02715", date: "060821"})
     end
-
-    it '#text_decrypter(message) uses shifts to decrypt' do
-      # need mock/stub for key and date generation
-      expect(@enigma.text_decrypter("nefau qdxly")).to eq("hello world")
-    end
   end
 
   describe 'helper methods (may be moved to other classes)' do
-    xit '#today_generator method returns date in DDMMYY format' do
-      # need mock/stub for date
-      expect(@enigma.today_generator).to eq("070821")
-    end
+
 
     it '#key_parser parses keys' do
       expected = {a_key: 02, b_key: 27, c_key: 71, d_key: 15}
@@ -82,64 +68,5 @@ RSpec.describe Enigma do
       expect(@enigma.offset_generator("040895")).to eq(supplied_date_offsets)
     end
 
-    xit '#shift_generator adds keys and offsets together' do
-      date1 = double(@enigma.date)
-      allow(date1).to receive(:date).and_return("070821")
-      key1 = double(@enigma.key_generator)
-      allow(key1).to receive(:key_generator).and_return("71285")
-      # @enigma.offset_generator("070821")
-      # @enigma.key_parser("71285")
-      expected = {a_shift: 48, b_shift: 45, c_shift: 58, d_shift: 48}
-      expect(@enigma.shift_generator).to eq(expected)
-    end
-
-    # it '#char_index_lookup returns index of each message char in character_array' do
-    #   expected = [7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3]
-    #
-    #   expect(@enigma.char_lookup("hello world")).to eq(expected)
-    # end
-
-    it '#message_char_shift_groups(message) collects character  according to which shift should be applied' do
-      expected = [["h", "o", "r"], ["e", " ", "l"], ["l", "w", "d"], ["l", "o"]]
-
-      expect(@enigma.message_char_shift_groups("hello world")).to eq(expected)
-    end
-
-    it '#shift_rotation(shift) returns shifted character array' do
-      # shift_1 = ["z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"]
-      # shift_2 = ["n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
-
-      # need mock/stub for shifts
-      # a_rotation_hash = {"a"=>"i",
-      #                    "b"=>"j",
-      #                    "c"=>"k",
-      #                    "d"=>"l",
-      #                    "e"=>"m",
-      #                    "f"=>"n",
-      #                    "g"=>"o",
-      #                    "h"=>"p",
-      #                    "i"=>"q",
-      #                    "j"=>"r",
-      #                    "k"=>"s",
-      #                    "l"=>"t",
-      #                    "m"=>"u",
-      #                    "n"=>"v",
-      #                    "o"=>"w",
-      #                    "p"=>"x",
-      #                    "q"=>"y",
-      #                    "r"=>"z",
-      #                    "s"=>" ",
-      #                    "t"=>"a",
-      #                    "u"=>"b",
-      #                    "v"=>"c",
-      #                    "w"=>"d",
-      #                    "x"=>"e",
-      #                    "y"=>"f",
-      #                    "z"=>"g",
-      #                    " "=>"h"}
-
-      expect(@enigma.shift_rotation(:a_shift)).to be_a(Hash)
-      expect(@enigma.shift_rotation(:a_shift).length).to eq(27)
-    end
   end
 end
